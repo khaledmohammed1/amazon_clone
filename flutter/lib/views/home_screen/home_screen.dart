6a045749a89870e9e7c186b95b99060f601f8457
khaled <kha103led@yahoo.com>
2022-07-11 14:16:04 +0200

    continue development

--- flutter/lib/views/home_screen/home_screen.dart
+++ flutter/lib/views/home_screen/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:amazon_clone/controllers/user_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,10 +11,9 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    UserController userController =UserController();
     return Scaffold(
       appBar: AppBar(title: const Text("Home"),centerTitle: true,),
-      body: Center(child: Text(userController.id.toString()),),
+
     );
   }
 }
